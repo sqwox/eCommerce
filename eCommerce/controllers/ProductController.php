60d928582e9552f739e5747443a853cfd199943c
sqwox <84541202+sqwox@users.noreply.github.com>
2021-07-19 23:28:10 +0300

    kl

--- eCommerce/controllers/ProductController.php
+++ eCommerce/controllers/ProductController.php
@@ -7,6 +7,7 @@ class ProductController extends AppController{
 public function actionView(){
     $id =Yii::$app->request->get('id');
     $product = Product::findOne($id);
+    $this->setMeta('E-SHOPER | ' . $product->name, $product->keywords, $product->description);
     return $this->render('view', compact('product'));
 }
 }
--- eCommerce/views/product/view.php
+++ eCommerce/views/product/view.php
@@ -23,27 +23,27 @@ use yii\helpers\Url;
 					<div class="product-details"><!--product-details-->
 						<div class="col-sm-5">
 							<div class="view-product">
-								<img src="images/product-details/1.jpg" alt="" />
-								<h3>ZOOM</h3>
+								 <?= Html::img("@web/images/products/{$product->img}", ['alt' => $product->name])?>
+								 <h3>ZOOM</h3>
 							</div>
 							<div id="similar-product" class="carousel slide" data-ride="carousel">
 								
 								  <!-- Wrapper for slides -->
 								    <div class="carousel-inner">
 										<div class="item active">
-										  <a href=""><img src="images/product-details/similar1.jpg" alt=""></a>
-										  <a href=""><img src="images/product-details/similar2.jpg" alt=""></a>
-										  <a href=""><img src="images/product-details/similar3.jpg" alt=""></a>
+										  <a href=""><img src="/images/product-details/similar1.jpg" alt=""></a>
+										  <a href=""><img src="/images/product-details/similar2.jpg" alt=""></a>
+										  <a href=""><img src="/images/product-details/similar3.jpg" alt=""></a>
 										</div>
 										<div class="item">
-										  <a href=""><img src="images/product-details/similar1.jpg" alt=""></a>
-										  <a href=""><img src="images/product-details/similar2.jpg" alt=""></a>
-										  <a href=""><img src="images/product-details/similar3.jpg" alt=""></a>
+										  <a href=""><img src="/images/product-details/similar1.jpg" alt=""></a>
+										  <a href=""><img src="/images/product-details/similar2.jpg" alt=""></a>
+										  <a href=""><img src="/images/product-details/similar3.jpg" alt=""></a>
 										</div>
 										<div class="item">
-										  <a href=""><img src="images/product-details/similar1.jpg" alt=""></a>
-										  <a href=""><img src="images/product-details/similar2.jpg" alt=""></a>
-										  <a href=""><img src="images/product-details/similar3.jpg" alt=""></a>
+										  <a href=""><img src="/images/product-details/similar1.jpg" alt=""></a>
+										  <a href=""><img src="/images/product-details/similar2.jpg" alt=""></a>
+										  <a href=""><img src="/images/product-details/similar3.jpg" alt=""></a>
 										</div>
 										
 									</div>
@@ -60,12 +60,18 @@ use yii\helpers\Url;
 						</div>
 						<div class="col-sm-7">
 							<div class="product-information"><!--/product-information-->
-								<img src="images/product-details/new.jpg" class="newarrival" alt="" />
-								<h2>Anne Klein Sleeveless Colorblock Scuba</h2>
+				
+								<?php if($product->new): ?> 
+			   <?= Html::img("@web/images/home/new.png", [ 'alt' => 'Новинка', 'class' => 'newarrival' ]) ?>
+               <?php endif; ?>
+               <?php if($product->sale): ?>
+               <?= Html::img("@web/images/home/sale.png", [ 'alt' => 'Распродажа', 'class' => 'newarrival' ]) ?>
+               <?php endif; ?>
+								<h2><?= $product->name ?></h2>
 								<p>Web ID: 1089772</p>
-								<img src="images/product-details/rating.png" alt="" />
+								<img src="/images/product-details/rating.png" alt="" />
 								<span>
-									<span>US $59</span>
+									
 									<label>Quantity:</label>
 									<input type="text" value="3" />
 									<button type="button" class="btn btn-fefault cart">
@@ -74,8 +80,8 @@ use yii\helpers\Url;
 									</button>
 								</span>
 								<p><b>Availability:</b> In Stock</p>
-								<p><b>Condition:</b> New</p>
-								<p><b>Brand:</b> E-SHOPPER</p>
+								
+							
 								<a href=""><img src="images/product-details/share.png" class="share img-responsive"  alt="" /></a>
 							</div><!--/product-information-->
 						</div>
